236a02a9eb661e23b92b3c7635748144e7ae33c7
Anton Tsigularov <anton@xeneta.com>
2016-06-10 13:39:21 +0200

    Patch missing function return value and PEP8 cleanup

--- qualify_leads/run.py
+++ qualify_leads/run.py
@@ -1,13 +1,13 @@
-import cPickle
 import re
-import numpy as np
-import xlrd, xlwt
+import xlrd
+import xlwt
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
 from sklearn.externals import joblib
 
 stemmer = SnowballStemmer('english')
 stopWords = set(stopwords.words('english'))
+
 
 def getSheet(name):
     sheets = xlrd.open_workbook(name)
@@ -16,8 +16,9 @@ def getSheet(name):
 
 vectorizer = joblib.load("algorithms/vectorizer")
 forest = joblib.load("algorithms/forest")
-###gnb=joblib.load("algorithms/gnb")
+# gnb=joblib.load("algorithms/gnb")
 tfidf_transformer = joblib.load("algorithms/tfidf_transformer")
+
 
 def cleanUp(descriptions):
     clean_descriptions = []
@@ -27,9 +28,12 @@ def cleanUp(descriptions):
         for word in desc.split():
             stemmed_word = stemmer.stem(word)
             stemmed_desc.append(stemmed_word)
-        meaningful_desc = [word.lower() for word in stemmed_desc if not word in stopWords]
+        meaningful_desc = [
+            word.lower() for word in stemmed_desc if word not in stopWords
+        ]
         clean_descriptions.append(" ".join(meaningful_desc))
     return clean_descriptions
+
 
 def getUrlsFromSheet(sheet):
     urls = []
@@ -38,6 +42,7 @@ def getUrlsFromSheet(sheet):
         urls.append(url)
     return urls
 
+
 def getDescriptionsFromSheet(sheet):
     descriptions = []
     for index in xrange(1, sheet.nrows):
@@ -45,8 +50,10 @@ def getDescriptionsFromSheet(sheet):
         descriptions.append(description)
     return descriptions
 
+
 def transform(data, transformer):
     return transformer.transform(data).toarray()
+
 
 def saveData(descriptions, urls, predictions):
     wb = xlwt.Workbook()
@@ -61,15 +68,18 @@ def saveData(descriptions, urls, predictions):
         ws.write(index + 1, 2, predictions[index])
     wb.save('output/predictions.xls')
 
+
 def qualifyLeads():
     sheet = getSheet('input/data.xlsx')
     descriptions = getDescriptionsFromSheet(sheet)
     urls = getUrlsFromSheet(sheet)
     clean_descriptions = cleanUp(descriptions)
     vectorized_descriptions = transform(clean_descriptions, vectorizer)
-    transformed_descriptions = transform(vectorized_descriptions, tfidf_transformer)
+    transformed_descriptions = transform(
+        vectorized_descriptions, tfidf_transformer
+    )
     predictions = forest.predict(transformed_descriptions)
-    ###predictions = gnb.predict(transformed_descriptions)
+    # predictions = gnb.predict(transformed_descriptions)
     saveData(descriptions, urls, predictions)
 
 qualifyLeads()
--- train_algorithm/run.py
+++ train_algorithm/run.py
@@ -1,82 +1,92 @@
-import requests
 import re
-import simplejson as json
-import urllib2
 import cPickle
-import nltk
-import xlrd, xlwt
+import xlrd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.cross_validation import train_test_split
-###from sklearn.naive_bayes import GaussianNB
+# from sklearn.naive_bayes import GaussianNB
+
 from sklearn import neighbors
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
-from sklearn.metrics import precision_recall_fscore_support
+
 stemmer = SnowballStemmer('english')
 stopWords = set(stopwords.words('english'))
-vectorizer = CountVectorizer(analyzer = 'word', max_features=10000, ngram_range=(1, 3))
+vectorizer = CountVectorizer(
+    analyzer='word', max_features=10000, ngram_range=(1, 3)
+)
 tfidf_transformer = TfidfTransformer(norm='l1', use_idf=True)
+
 
 def getSheet(name):
     sheets = xlrd.open_workbook(name)
     sheet = sheets.sheet_by_index(0)
     return sheet
 
+
 def cleanUp(descriptions, disc=False):
     clean_descriptions = []
     index = 0
-    length = len(descriptions)
+
     for desc in descriptions:
         desc = re.sub("[^a-zA-Z]", " ", desc)
         stemmed_desc = []
         for word in desc.split():
             stemmed_word = stemmer.stem(word)
             stemmed_desc.append(stemmed_word)
-        meaningful_desc = [word for word in stemmed_desc if not word in stopWords]
+        meaningful_desc = [
+            word for word in stemmed_desc if word not in stopWords
+        ]
         clean_descriptions.append(" ".join(meaningful_desc))
         index += 1
     return clean_descriptions
+
 
 def getDescriptionsFromSheet(sheet):
     descriptions = []
     for index in xrange(1, sheet.nrows):
         description = sheet.cell_value(index, 1)
         descriptions.append(description)
-    return descriptions   
+    return descriptions
+
 
 def transformByTfIdf(X_train, X_test, transformer):
     X_train = transformer.transform(X_train).toarray()
     X_test = transformer.transform(X_test).toarray()
     return X_train, X_test
 
+
 def transformByVectorizer(X_train, X_test, vectorizer):
     X_train = vectorizer.transform(X_train).toarray()
     X_test = vectorizer.transform(X_test).toarray()
     return X_train, X_test
 
+
 def vectorize(descriptions):
     vectorized_descriptions = vectorizer.fit_transform(descriptions)
     return vectorized_descriptions.toarray(), vectorizer
+
 
 def runForest(X_train, X_test, Y_train, Y_test):
     forest = RandomForestClassifier(n_estimators=50, random_state=1)
     forest = forest.fit(X_train, Y_train)
     score = forest.score(X_test, Y_test)
-    return score
+    return score, forest
 
-###def runGaussianNB(X_train, X_test, Y_train, Y_test):
-###    clf=GaussianNB()
-###    clf.fit(X_train,Y_train)
-###    score=clf.score(X_test, Y_test)
-###    return clf, score
+
+# def runGaussianNB(X_train, X_test, Y_train, Y_test):
+#    clf=GaussianNB()
+#    clf.fit(X_train,Y_train)
+#    score=clf.score(X_test, Y_test)
+#    return clf, score
 
 def runKNN(X_train, X_test, Y_train, Y_test):
     knn = neighbors.KNeighborsClassifier()
     knn.fit(X_train, Y_train)
-    score = knn.score(X_test,Y_test)
+    score = knn.score(X_test, Y_test)
     return score
+
 
 def createLabelArray(qualified, disqualified):
     labels = []
@@ -94,29 +104,31 @@ disqualified_descriptions = getDescriptionsFromSheet(disqualified_sheet)
 disqualified_clean_descriptions = cleanUp(disqualified_descriptions, True)
 
 X = qualified_clean_descriptions + disqualified_clean_descriptions
-Y = createLabelArray(qualified_clean_descriptions, disqualified_clean_descriptions)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+Y = createLabelArray(
+    qualified_clean_descriptions, disqualified_clean_descriptions
+)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=42
+)
 vectorizer.fit(X_train)
 X_train, X_test = transformByVectorizer(X_train, X_test, vectorizer)
 tfidf_transformer.fit(X_train)
 X_train, X_test = transformByTfIdf(X_train, X_test, tfidf_transformer)
 
-forest_score = runForest(X_train, X_test, Y_train, Y_test)
-###gnb, gnb_score=runGaussianNB(X_train, X_test, Y_train, Y_test)
+# gnb, gnb_score=runGaussianNB(X_train, X_test, Y_train, Y_test)
+forest_score, forest = runForest(X_train, X_test, Y_train, Y_test)
+
 print 'Random Forest score: ', forest_score
-###print 'Random Gaussian Naive Bayes score: ', gnb_score
+# print 'Random Gaussian Naive Bayes score: ', gnb_score
 
 with open('forest', 'wb') as f:
     cPickle.dump(forest, f)
 
-###with open('gnb', 'wb') as fi:
-###    cPickle.dump(gnb, fi)
+# with open('gnb', 'wb') as fi:
+#    cPickle.dump(gnb, fi)
 
 with open('vectorizer', 'wb') as file:
     cPickle.dump(vectorizer, file)
 
 with open('tfidf_transformer', 'wb') as file:
     cPickle.dump(tfidf_transformer, file)
-
-
-
